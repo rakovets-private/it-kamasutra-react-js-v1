74da301ccca9d46b53e7e92eed6b412f38ccac0c
Dmitry Rakovets <dmitryrakovets@gmail.com>
2022-02-15 07:22:39 +0300

    07. Уроки React JS (что такое Компонента)

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -4,17 +4,34 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <div>one div</div>
-      <div className="App">
-        <ul>
-          <li>css</li>
-          <li>html</li>
-          <li>js</li>
-          <li>react</li>
-        </ul>
-      </div>
+      <Header/>
+      <Technologies/>
     </div>
   );
+}
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  );
+}
+
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#">Home</a>
+      <a href="#">News Feed</a>
+      <a href="#">Messages</a>
+    </div>
+  )
 }
 
 export default App;
