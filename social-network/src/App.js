529e49d79e7b531a4a5d9d7c47bf63a538d8612c
Dmitry Rakovets <dmitryrakovets@gmail.com>
2022-02-17 06:32:54 +0300

    11. Уроки React JS (соц. сеть, шаг 1, вёрстка, grid)

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -1,13 +1,55 @@
 import React from 'react';
-import Technologies from "./Technologies";
-import Header from "./Header";
 import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-      <Technologies/>
+    <div className={"app-wrapper"}>
+      <header className="header">
+        <div className={"header-logo"}>
+          <img src={"https://cryptologos.cc/logos/uniswap-uni-logo.svg?v=021"}/>
+        </div>
+      </header>
+      <nav className={"nav"}>
+        <div className={"nav-item"}>
+          <a>Profile</a>
+        </div>
+        <div className={"nav-item"}>
+          <a>Messages</a>
+        </div>
+        <div className={"nav-item"}>
+          <a>News</a>
+        </div>
+        <div className={"nav-item"}>
+          <a>Music</a>
+        </div>
+        <div className={"nav-item"}>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <main className={"content"}>
+        <div className={"content-preview"}>
+        </div>
+        <div className={"content-profile"}>
+          <div className={"content-profile-avatar"}>
+            <img alt="avatar" src={"https://images.squarespace-cdn.com/content/v1/58b4791ad2b857c893179e34/1537982923668-VL76J6S6ZAY9IXOG0F84/IMG_2849.jpg?format=1500w"}/>
+          </div>
+          <div className={"content-profile-description"}>
+            My description
+          </div>
+        </div>
+        <div className={"content-post"}>
+          My posts
+          <div className={"content-post-sender"}>
+            new post
+          </div>
+          <div className={"content-post-item"}>
+            post 1
+          </div>
+          <div className={"content-post-item"}>
+            post 2
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
