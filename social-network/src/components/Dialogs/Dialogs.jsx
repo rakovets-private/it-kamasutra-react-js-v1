f239d0e864b3d2cdb2ed2079262dfdd992c99358
Dmitry Rakovets <dmitryrakovets@gmail.com>
2022-02-23 07:22:57 +0300

    21. Уроки React JS (Верстаем страницу Диалогов)

--- social-network/src/components/Dialogs/Dialogs.jsx
+++ social-network/src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-import style from "./Dialogs.module.css"
+import s from "./Dialogs.module.css"
+
+const SelectedLink = () => {
+  return (
+    select => select.isActive ? s.activeLink : s.dialog
+  )
+};
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogItems}>
+        <div className={SelectedLink()}>Ivan</div>
+        <div className={SelectedLink()}>Daniel</div>
+        <div className={SelectedLink()}>Viktor</div>
+        <div className={SelectedLink()}>Valentina</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>Hello</div>
+        <div className={s.message}>It's me!</div>
+        <div className={s.message}>Cool</div>
+      </div>
     </div>
   )
 }
