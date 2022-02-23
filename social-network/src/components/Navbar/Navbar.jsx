5b5d62eae7c7604ca16048ecc9678bac60c1ea12
Dmitry Rakovets <dmitryrakovets@gmail.com>
2022-02-23 06:56:19 +0300

    20. Уроки React JS (NavLink)

--- social-network/src/components/Navbar/Navbar.jsx
+++ social-network/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import style from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
+
+const SelectedLink = () => {
+  return (
+    select => select.isActive ? style.activeLink : style.item
+  )
+};
 
 const Navbar = () => {
   return (
     <nav className={style.nav}>
       <div className={style.item}>
-        <a href="/profile">Profile</a>
+        <NavLink to="/profile" className={SelectedLink()}>Profile</NavLink>
       </div>
       <div className={style.item}>
-        <a href="/dialogs">Messages</a>
+        <NavLink to="/dialogs" className={SelectedLink()}>Messages</NavLink>
       </div>
       <div className={style.item}>
-        <a href={"#news"}>News</a>
+        <NavLink to="/news" className={SelectedLink()}>News</NavLink>
       </div>
       <div className={style.item}>
-        <a href={"#music"}>Music</a>
+        <NavLink to="/music" className={SelectedLink()}>Music</NavLink>
       </div>
       <div className={style.item}>
-        <a href={"#settings"}>Settings</a>
+        <NavLink to="/settings" className={SelectedLink()}>Settings</NavLink>
       </div>
     </nav>
   )
