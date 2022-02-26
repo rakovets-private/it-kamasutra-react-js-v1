ccb204b7349c976de7985af04a195eed97eb747d
Dmitry Rakovets <dmitryrakovets@gmail.com>
2022-02-26 06:46:27 +0300

    24. Уроки React JS (UI - BLL, выносим данные в сторону)

--- social-network/src/components/Dialogs/Dialogs.jsx
+++ social-network/src/components/Dialogs/Dialogs.jsx
@@ -3,19 +3,32 @@ import s from "./Dialogs.module.css"
 import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
+  let dialogsData = [
+    {id: 1, name: "Ivan"},
+    {id: 2, name: "Daniel"},
+    {id: 3, name: "Viktor"},
+    {id: 4, name: "Valentina"},
+  ];
+  let messageData = [
+    {id: 1, message: "Hi"},
+    {id: 2, message: "Hello"},
+    {id: 3, message: "It's me!"},
+    {id: 4, message: "Cool"},
+  ]
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogItems}>
-        <Dialog name="Ivan" id="1"/>
-        <Dialog name="Daniel" id="2"/>
-        <Dialog name="Viktor" id="3"/>
-        <Dialog name="Valentina" id="4"/>
+        <Dialog name={dialogsData[0].name} id={dialogsData[0].id}/>
+        <Dialog name={dialogsData[1].name} id={dialogsData[1].id}/>
+        <Dialog name={dialogsData[2].name} id={dialogsData[2].id}/>
+        <Dialog name={dialogsData[3].name} id={dialogsData[3].id}/>
       </div>
       <div className={s.messages}>
-        <Message message="Hi"/>
-        <Message message="Hello"/>
-        <Message message="It's me!"/>
-        <Message message="Cool"/>
+        <Message message={messageData[0].message}/>
+        <Message message={messageData[1].message}/>
+        <Message message={messageData[2].message}/>
+        <Message message={messageData[3].message}/>
       </div>
     </div>
   )
--- social-network/src/components/Profile/MyPosts/MyPosts.jsx
+++ social-network/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,12 @@ import Post from "./Post/Post";
 import s from "./MyPosts.module.css"
 
 const MyPosts = () => {
+  let postData = [
+    {id: 1, message: "Hi, how are you?", countLike: 1},
+    {id: 2, message: "It's my first post", countLike: 2},
+    {id: 3, message: "WTF?!", countLike: 3},
+    {id: 4, message: "Oops!", countLike: 7},
+  ]
   return (
     <div className={s.posts}>
       My posts
@@ -10,10 +16,10 @@ const MyPosts = () => {
         <textarea></textarea><br/>
         <button>Add post</button>
       </div>
-      <Post message="Hi, how are you?" countLike="2"/>
-      <Post message="It's my first post" countLike="2"/>
-      <Post message="WTF?!" countLike="2"/>
-      <Post message="Oops!" countLike="2"/>
+      <Post message={postData[0].message} countLike={postData[0].countLike}/>
+      <Post message={postData[1].message} countLike={postData[1].countLike}/>
+      <Post message={postData[2].message} countLike={postData[2].countLike}/>
+      <Post message={postData[3].message} countLike={postData[3].countLike}/>
     </div>
   )
 }
