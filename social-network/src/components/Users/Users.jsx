3434d03524b89217029b4374186451bbc668ed05
Dmitry Rakovets <dmitryrakovets@gmail.com>
2022-03-23 07:28:42 +0300

    53 - React JS практика - классовый компонент (extends React.Component)

--- social-network/src/components/Users/Users.jsx
+++ social-network/src/components/Users/Users.jsx
@@ -2,46 +2,26 @@ import React from 'react';
 import s from './Users.module.css';
 import * as axios from 'axios';
 
-const Users = (props) => {
-  function getUsers() {
-    if (props.users.length === 0) {
-      axios.get("https://social-network.samuraijs.com/api/1.0/users")
-        .then(response => {
-          console.log(response.data.items);
-          props.addUsers(response.data.items);
-        })
-        .catch(
-          reason => {
-            console.log(reason);
-          }
-        )
-    }
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getUsers();
   }
-
-  let users = [
-    {
-      id: 5,
-      followed: false,
-      shortname: 'Elena A.',
-      status: '...',
-      location: {country: 'Belarus', city: 'Brest'},
-      avatarUrl: "https://kb.rspca.org.au/wp-content/uploads/2018/11/golder-retriever-puppy.jpeg"
-    }
-  ]
   
-  return (
-    <div>
-      {
-        props.users.map(u => <div key={u.id}>
+  render() {
+    return (
+      <div>
+        {
+          this.props.users.map(u => <div key={u.id}>
           <span>
             <div><img className={s.img} src={u.avatarUrl} alt="avatar"/></div>
             <div>
               {u.followed
-                ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button> 
-                : <button onClick={() => props.follow(u.id)}>Follow</button>}
+                ? <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button>
+                : <button onClick={() => this.props.follow(u.id)}>Follow</button>}
             </div>
           </span>
-          <span>
+            <span>
             <span>
               <div>{u.name}</div>
               <div>{u.status}</div>
@@ -52,11 +32,27 @@ const Users = (props) => {
               <div>{"u.location.city"}</div>
             </span>
           </span>
-        <br/></div>)
-      }
-      <button onClick={() => getUsers()}>Show more</button>
-    </div>
-  )
+            <br/></div>)
+        }
+        <button onClick={() => this.getUsers()}>Show more</button>
+      </div>
+    )
+  }
+
+  getUsers = () => {
+    if (this.props.users.length === 0) {
+      axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        .then(response => {
+          console.log(response.data.items);
+          this.props.addUsers(response.data.items);
+        })
+        .catch(
+          reason => {
+            console.log(reason);
+          }
+        )
+    }
+  }
 }
 
 export default Users;
