9cbe37e61fbdc8b820e618bb40a81ce3427be779
Dmitry Rakovets <dmitryrakovets@gmail.com>
2022-03-31 07:21:53 +0300

    62 - React JS - практика, follow-unfollow api

--- social-network/src/components/Users/Users.jsx
+++ social-network/src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import avatar from './../../assets/images/profile-default.png'
 import s from './Users.module.css';
 import {NavLink} from 'react-router-dom';
+import axios from 'axios';
 
 let Users = (props) => {
   let pageCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -33,8 +34,40 @@ let Users = (props) => {
             </div>
             <div>
               {u.followed
-                ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
-                : <button onClick={() => props.follow(u.id)}>Follow</button>}
+                ? <button onClick={() => {
+                  axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+                    {
+                      withCredentials: true,
+                      headers: {'API-KEY': '4c2a14f4-de4f-467d-ad92-6dffcdc2ea9d'}
+                    })
+                    .then(response => {
+                      if (response.data.resultCode === 0) {
+                        props.unfollow(u.id);
+                      }
+                    })
+                    .catch(
+                      reason => {
+                        console.log(reason);
+                      }
+                    )
+                }}>Unfollow</button>
+                : <button onClick={() => {
+                  axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {},
+                    {
+                      withCredentials: true,
+                      headers: {'API-KEY': '4c2a14f4-de4f-467d-ad92-6dffcdc2ea9d'}
+                    })
+                    .then(response => {
+                      if (response.data.resultCode === 0) {
+                        props.follow(u.id);
+                      }
+                    })
+                    .catch(
+                      reason => {
+                        console.log(reason);
+                      }
+                    )
+                }}>Follow</button>}
             </div>
           </span>
           <span>
