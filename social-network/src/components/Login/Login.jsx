1fc2746dfa0aaf6a03616bc7819db5a0c8f73cc1
Dmitry Rakovets <dmitryrakovets@gmail.com>
2022-04-07 08:03:28 +0300

    75 - React JS - redux-form введение (login)

--- social-network/src/components/Login/Login.jsx
+++ social-network/src/components/Login/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import {Field, reduxForm} from 'redux-form';
 
-const Login = (props) => {
-  return (
-    <h2>Login</h2>
+const Login = () => {
+  let login = (formData) => {
+    console.log(formData);
+  }
+  
+  return (<>
+      <h2>Login</h2>
+      <LoginReduxForm onSubmit={login}/>
+    </>
   )
 }
+
+const LoginForm = (props) => {
+  return (<form onSubmit={props.handleSubmit}>
+    <div><Field name='login' component='input' placeholder={'Login'}/></div>
+    <div><Field name='password' component='input' type='password' placeholder={'Password'}/></div>
+    <div><label><Field name='rememberMe' component='input' type='checkbox'/>Remember me</label></div>
+    <div><input type='submit' value={'Sign in'}/></div>
+  </form>)
+}
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm);
 
 export default Login;
