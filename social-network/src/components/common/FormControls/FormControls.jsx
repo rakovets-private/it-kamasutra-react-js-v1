b5445c1dd5ab18f4c82074f53f93a11c75adbe17
Dmitry Rakovets <dmitryrakovets@gmail.com>
2022-04-19 04:52:02 +0300

    fix: error when run app

--- social-network/src/components/common/FormControls/FormControls.jsx
+++ social-network/src/components/common/FormControls/FormControls.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import s from './FormControls.module.css'
 
 export const Element = ({input, meta, ...props}) => {
-  const hasError = meta.touched && meta.error;
+  const hasError = meta && meta.touched && meta.error;
   return (<div className={s.formControl + ' ' + (hasError ? s.error : '')}>
     <div>
-      <props.elementType {...props} {...input}/>
+      <props.elementtype {...props} {...input}/>
     </div>
     {hasError && <div><span>{meta.error}</span></div>}
   </div>)
 }
 
 export const Textarea = (props) => {
-  return (<Element {...props} elementType='textarea'/>)
+  return (<Element {...props} elementtype='textarea'/>)
 }
 
 export const Input = ({input, meta, ...props}) => {
-  return (<Element {...props} elementType='input'/>)
+  return (<Element {...props} elementtype='input'/>)
 }
